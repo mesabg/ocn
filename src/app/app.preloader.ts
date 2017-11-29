2e41fcd37e2261992f1e685c5b3ab6ef5ce58a38
Moisés Berenguer <moises.berenguer@gmail.com>
2017-11-29 02:41:33 -0400

    LC

--- src/app/app.preloader.ts
+++ src/app/app.preloader.ts
@@ -1,11 +1,10 @@
-import { PreloadingStrategy, Route } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
-export class AppPreloader implements PreloadingStrategy {
-  preload(route: Route, load: Function): Observable<any> {
+export class AppPreloader {
+  /*preload(route: Route, load: Function): Observable<any> {
 		console.log("Preloading things");
     return route.data && route.data.preload ? load() : of(null);
-  }
+  }*/
 }
